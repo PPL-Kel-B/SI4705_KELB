c0a237165e4f0294454f2a11aa1feeefea1ad2b9
fiyolalmnda <fiyolanuralamanda@gmail.com>
2026-04-25 21:43:30 +0700

    PBI-17 Final Code Login

--- sharebite/routes/web.php
+++ sharebite/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',  [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');  
+
+Route::view('/login', 'login');
