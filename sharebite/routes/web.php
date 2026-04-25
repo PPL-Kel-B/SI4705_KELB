c115b1b52fd127d4b3b1a290ef05847f29944fd0
FrdMnhdl <faridzaridzaridzarid@gmail.com>
2026-04-25 16:45:46 +0700

    Dari 0 lagi

--- sharebite/routes/web.php
+++ sharebite/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegisterUnitBisnisController;
 
-Route::get('/register/unit-bisnis', [RegisterUnitBisnisController::class, 'index'])->name('register.unit-bisnis');
-Route::post('/register/unit-bisnis', [RegisterUnitBisnisController::class, 'store'])->name('register.unit-bisnis.store');
+Route::get('/', function () {
+    return view('welcome');
+});
