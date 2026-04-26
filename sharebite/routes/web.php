297d0644f1e82c5e7a6349d8efd47bcf6fd1658a
alyanisywahhandani <alyaniswah2005@gmail.com>
2026-04-26 15:20:23 +0700

    feat:profile page

--- sharebite/routes/web.php
+++ sharebite/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
